a4b09f03fd25df35dd96e72eb65059112e8ac214
Anuj Upadhyay <openmindedlearning@gmail.com>
2023-11-22 22:35:14 +0530

    edx-web-01-py - basics.

---
+++ python-basics/hello.py
@@ -0,0 +1,23 @@
+print('Hello, world!')
+
+# Variables
+a = 28          # int
+b = 1.5         # float
+c = "Hello!"    # str
+d = True        # bool
+e = None        # NoneType
+
+# formatting
+name = input("Name: ")
+print(f"Hello, {name}")
+
+# conditions
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is not positive")
+else:
+    print("n is zero")
+
